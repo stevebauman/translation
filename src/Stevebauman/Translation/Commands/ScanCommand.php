763068c8c2ed743c14b8ba54f01dfc9fb168f52f
Steve Bauman <steven_bauman@outlook.com>
2015-03-25 10:55:34 -0400

    Small scan command tweak

--- src/Stevebauman/Translation/Commands/ScanCommand.php
+++ src/Stevebauman/Translation/Commands/ScanCommand.php
@@ -220,7 +220,7 @@ class ScanCommand extends Command {
 
         foreach ($cdir as $key => $value)
         {
-            if (!in_array($value, array(".", "..")))
+            if ( ! in_array($value, array(".", "..")))
             {
                 if (is_dir($dir . DIRECTORY_SEPARATOR . $value))
                 {
